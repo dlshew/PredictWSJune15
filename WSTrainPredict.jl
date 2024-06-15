e7bf46aae5add7b141560a297ac2d0304a903aff
Dylan Shew <49965772+dlshew@users.noreply.github.com>
2024-06-14 20:01:57 -0500

    Add files via upload

---
+++ WSTrainPredict.jl
@@ -0,0 +1,31 @@
+using CSV, DataFrames, Random, MLJ
+
+WS = CSV.read("CleanWS.csv", DataFrame)
+TwoFour = CSV.read("Clean24.csv", DataFrame)
+
+RandomForestClass = @load RandomForestClassifier pkg=DecisionTree
+model = RandomForestClass(max_depth=5, n_trees=500)
+
+shuffle!(WS)
+Train, Test = partition(WS, .70)
+yTrain, XTrain = unpack(Train, ==(:WonWS))
+yTest, XTest = unpack(Test, ==(:WonWS))
+yTrain = coerce(yTrain, Multiclass)
+
+mach = machine(model, XTrain[:, 2:25], yTrain) |> fit!
+yhat = predict(mach, XTest[:, 2:25])
+display([yTest yhat])
+println(log_loss(yhat, yTest)) 
+
+Prediction = predict(mach, TwoFour[:, 2:25])
+TwoFour.WS .= broadcast(pdf, Prediction, 1)
+TwoFour.WS .= (TwoFour.WS ./ sum(TwoFour.WS)) .* 100
+
+select!(TwoFour,  :Team, :WS)
+sort!(TwoFour, :WS, rev=true)
+
+CSV.write("WSProj5.csv", TwoFour)
+println(fitted_params(mach).forest)
+FImportant = DataFrame(feature_importances(mach))
+show(FImportant)
+#CSV.write("ParameterImportance.csv", FImportant)
